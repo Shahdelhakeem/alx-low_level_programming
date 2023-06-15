f1e3194a11f45d55440fff926451ad7e615b794e
shahd elhakeem <shootamer431@gmail.com>
2023-06-15 16:04:24 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
-#include <studio.h>
-int main(void){
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put functiom
+ *
+ * Return: Alawys 0 (success)
+ */
+
+int  main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
