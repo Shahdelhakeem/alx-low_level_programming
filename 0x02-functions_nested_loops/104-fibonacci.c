7f1b136a68dca9963373bf625e4a6a4ed1971b3c
shahd elhakeem <shootamer431@gmail.com>
2023-06-21 00:47:27 -0700

    a

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -41,7 +41,7 @@ int main(void)
 		if (f1o > 0)
 			printf("%lu", f1o);
 			initial0s = numLength(mx) - 1 - numLength(f1);
-		while
+		while(f10 > 0 && initial0s > 0)
 		{
 			printf("%d", 0)
 			initial0s--;
